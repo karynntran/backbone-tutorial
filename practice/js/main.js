a8b1833f46148dbef1bfa686b9aecfe4e919f9ec
karynntran <karynn.tran@gmail.com>
2015-01-18 17:26:11 -0500

    Complete Collections Project

--- practice/js/main.js
+++ practice/js/main.js
@@ -1,3 +1,5 @@
+// MODELS PROJECT #1//
+
 var Vehicle = Backbone.Model.extend({
 	urlRoot: "/api/vehicles",
 	validate: function(attrs){
@@ -15,8 +17,32 @@ var Car = Vehicle.extend({
 	},
 });
 
+// COLLECTIONS PROJECT #2//
+var Vehicles = Backbone.Collection.extend({
+	model: Vehicle,
+	url: "/api/vehicles"
+});
+
+var vehicles = new Vehicles();
+
+vehicles.add(new Vehicle({ car: 1, registrationNumber: "XLI887",colour: "Blue"}));
+vehicles.add(new Vehicle({ car: 2, registrationNumber: "ZNP123",colour: "Blue"}));
+vehicles.add(new Vehicle({ car: 3, registrationNumber: "XUV456",colour: "Gray"}));
+
 $(function() {
-	car = new Car({ registrationNumber: "XLI887", color: "blue" });
-	car.start();
-	console.log(car.isValid());
+	
+//PROJECT#2
+	console.log("All Vehicles",vehicles);
+	console.log("Blue", vehicles.where({colour: "Blue"}));
+	console.log("XLI887", vehicles.where({registrationNumber: "XLI887"}));
+	// vehicles.remove(vehicles.where({registrationNumber: "XLI887" }));
+	console.log(vehicles.toJSON());
+	vehicles.each(function(vehicle){
+		console.log("Iteration",vehicle);
+	})
+
+// PROJECT#1
+	// car = new Car({ registrationNumber: "XLI887", color: "blue" });
+	// car.start();
+	// console.log(car.isValid());
 });
